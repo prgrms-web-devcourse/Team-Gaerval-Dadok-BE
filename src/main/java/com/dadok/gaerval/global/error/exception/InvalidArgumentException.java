bb3b64f80e641df6c75f6afadc10a852a33d5fba
ysk <kim206gh@naver.com>
2023-02-26 19:55:09 +0900

    feat: invalidArgument 및 Validator 정의

---
+++ src/main/java/com/dadok/gaerval/global/error/exception/InvalidArgumentException.java
@@ -0,0 +1,9 @@
+package com.dadok.gaerval.global.error.exception;
+
+public class InvalidArgumentException extends BusinessException{
+
+	public InvalidArgumentException(String message) {
+		super(message);
+	}
+
+}
