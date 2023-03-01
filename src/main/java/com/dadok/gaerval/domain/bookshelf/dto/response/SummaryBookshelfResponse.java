a5ec280b864925e64d235cc9a9fb1c813d534db8
youngji <youngji.jang804@gmail.com>
2023-03-01 22:54:42 +0900

    fix : bookshelf repository 반환값 DTO에서 Entity로 변경

--- src/main/java/com/dadok/gaerval/domain/bookshelf/dto/response/SummaryBookshelfResponse.java
+++ src/main/java/com/dadok/gaerval/domain/bookshelf/dto/response/SummaryBookshelfResponse.java
@@ -2,22 +2,7 @@ package com.dadok.gaerval.domain.bookshelf.dto.response;
 
 import java.util.List;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-@Getter
-public class SummaryBookshelfResponse {
-
-	private final Long bookshelfId;
-
-	private final String bookshelfName;
-
-	private List<SummaryBookResponse> books;
-
-	public void setBooks(List<SummaryBookResponse> books) {
-		this.books = books;
-	}
+public record SummaryBookshelfResponse(Long bookshelfId, String bookshelfName, List<SummaryBookResponse> books) {
 
 	public record SummaryBookResponse(
 		Long bookId, String title, String imageUrl) {
