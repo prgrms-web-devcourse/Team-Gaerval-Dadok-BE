bddbd4ecce502227562d4289e1be78d4c34ef118
ysk <kim206gh@naver.com>
2023-03-05 06:41:17 +0900

    refactor : BookGroup 엔티티 수정 -
    title 컬럼 추가하고 인덱스 설정. GroupComment Entity 생성하면서 연관관계 맺음

--- src/main/java/com/dadok/gaerval/domain/book_group/entity/BookGroup.java
+++ src/main/java/com/dadok/gaerval/domain/book_group/entity/BookGroup.java
@@ -1,10 +1,11 @@
-package com.dadok.gaerval.domain.bookGroup.entity;
+package com.dadok.gaerval.domain.book_group.entity;
 
 import static com.dadok.gaerval.global.util.CommonValidator.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -13,12 +14,15 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Index;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.dadok.gaerval.domain.book.entity.Book;
+import com.dadok.gaerval.global.common.JacocoExcludeGenerated;
 import com.dadok.gaerval.global.common.entity.BaseTimeColumn;
+import com.dadok.gaerval.global.util.CommonValidator;
 
 import io.jsonwebtoken.lang.Assert;
 import lombok.AccessLevel;
@@ -26,7 +30,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "book_group")
+@Table(name = "book_groups",
+	indexes = {@Index(name = "owner_id_index", columnList = "owner_id"),
+		@Index(name = "is_public_index", columnList = "is_public")}
+)
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BookGroup extends BaseTimeColumn {
@@ -35,11 +42,14 @@ public class BookGroup extends BaseTimeColumn {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(nullable = false)
+	@Column(nullable = false, name = "owner_id")
 	private Long ownerId;
 
 	@OneToOne(fetch = FetchType.LAZY)
 	private Book book;
+
+	@Column(nullable = false, length = 30)
+	private String title;
 
 	@Column(nullable = false)
 	private LocalDate startDate;
@@ -53,17 +63,21 @@ public class BookGroup extends BaseTimeColumn {
 	@Column(nullable = false, length = 1000)
 	private String introduce;
 
-	@Column(nullable = false)
+	@Column(nullable = false, name = "is_public")
 	private Boolean isPublic;
 
 	@OneToMany(mappedBy = "bookGroup", cascade = CascadeType.ALL, orphanRemoval = true)
 	private final List<GroupMember> groupMembers = new ArrayList<>();
 
+	@OneToMany(mappedBy = "bookGroup", cascade = CascadeType.PERSIST, orphanRemoval = true)
+	private List<GroupComment> comments = new ArrayList<>();
+
 	protected BookGroup(Long ownerId, Book book, LocalDate startDate, LocalDate endDate, Integer maxMemberCount,
-		String introduce, Boolean isPublic) {
+		String introduce, Boolean isPublic, String title) {
 		validateNotnull(ownerId, "ownerId");
+		validateLengthLessThen(title, 30, "title");
 		validateNotnull(book, "book");
-		validatePeriod(startDate, endDate);
+		CommonValidator.validatePeriod(startDate, endDate);
 		validateMaxMemberCount(maxMemberCount);
 		validateLengthLessThen(introduce, 1000, "introduce");
 		validateNotnull(isPublic, "isPublic");
@@ -74,15 +88,7 @@ public class BookGroup extends BaseTimeColumn {
 		this.maxMemberCount = maxMemberCount;
 		this.introduce = introduce;
 		this.isPublic = isPublic;
-	}
-
-	private void validatePeriod(LocalDate startDate, LocalDate endDate) {
-		validateNotnull(startDate, "startDate");
-		validateNotnull(endDate, "endDate");
-		Assert.isTrue(startDate.isEqual(LocalDate.now()) || startDate.isAfter(LocalDate.now()),
-			"모임 시작 날짜는 오늘 부터 가능합니다.");
-		Assert.isTrue(endDate.isEqual(startDate) || endDate.isAfter(startDate),
-			String.format("모임 종료 날짜는 시작 날짜(%s)부터 가능합니다.", startDate));
+		this.title = title;
 	}
 
 	private void validateMaxMemberCount(Integer maxMemberCount) {
@@ -92,7 +98,45 @@ public class BookGroup extends BaseTimeColumn {
 
 	public static BookGroup create(Long ownerId, Book book, LocalDate startDate, LocalDate endDate,
 		Integer maxMemberCount,
+		String title,
 		String introduce, Boolean isPublic) {
-		return new BookGroup(ownerId, book, startDate, endDate, maxMemberCount, introduce, isPublic);
+		return new BookGroup(ownerId, book, startDate, endDate, maxMemberCount, introduce, isPublic, title);
+	}
+
+	public void addComment(GroupComment groupComment) {
+		validateNotnull(groupComment, "groupComment");
+		if (!this.comments.contains(groupComment)) {
+			this.comments.add(groupComment);
+		}
+	}
+
+	public void addMember(GroupMember groupMember) {
+		validateNotnull(groupMember, "groupMember");
+		if (!this.groupMembers.contains(groupMember)) {
+			this.groupMembers.add(groupMember);
+			groupMember.joinGroup(this);
+		}
+	}
+
+	@JacocoExcludeGenerated
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		BookGroup bookGroup = (BookGroup)o;
+		return Objects.equals(id, bookGroup.id) && Objects.equals(ownerId, bookGroup.ownerId)
+			&& Objects.equals(book, bookGroup.book) && Objects.equals(title, bookGroup.title)
+			&& Objects.equals(startDate, bookGroup.startDate) && Objects.equals(endDate,
+			bookGroup.endDate) && Objects.equals(maxMemberCount, bookGroup.maxMemberCount)
+			&& Objects.equals(introduce, bookGroup.introduce) && Objects.equals(isPublic,
+			bookGroup.isPublic);
+	}
+
+	@JacocoExcludeGenerated
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, ownerId, book, title, startDate, endDate, maxMemberCount, introduce, isPublic);
 	}
 }
---
+++ src/test/java/com/dadok/gaerval/domain/book_group/entity/BookGroupTest.java
@@ -0,0 +1,192 @@
+package com.dadok.gaerval.domain.book_group.entity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.dadok.gaerval.domain.book.entity.Book;
+import com.dadok.gaerval.domain.user.entity.User;
+import com.dadok.gaerval.global.error.exception.InvalidArgumentException;
+import com.dadok.gaerval.testutil.BookGroupObjectProvider;
+import com.dadok.gaerval.testutil.BookObjectProvider;
+import com.dadok.gaerval.testutil.GroupCommentObjectProvider;
+import com.dadok.gaerval.testutil.UserObjectProvider;
+
+class BookGroupTest {
+
+	private final Book book = BookObjectProvider.createRequiredFieldBook();
+
+	@DisplayName("create - bookGroup 생성 - 성공")
+	@Test
+	void create_success() {
+		assertDoesNotThrow(() -> {
+			BookGroup.create(2L, book, LocalDate.now(), LocalDate.now(),
+				2, "작은 모임", "작은 모임", true);
+		});
+	}
+
+	@DisplayName("create - userId null일 경우 - 실패")
+	@Test
+	void create_userIdNull_fail() {
+		assertThrows(InvalidArgumentException.class, () -> {
+			BookGroup.create(null, book, LocalDate.now(), LocalDate.now(),
+				2, "작은 모임", "작은 모임", true);
+		});
+	}
+
+	@DisplayName("create - title null일 경우 - 실패")
+	@Test
+	void create_titleNull_fail() {
+		assertThrows(InvalidArgumentException.class, () -> {
+			BookGroup.create(null, book, LocalDate.now(), LocalDate.now(),
+				2, null, "작은 모임", true);
+		});
+	}
+
+	@DisplayName("create - title 30자 이상일 경우 - 실패")
+	@Test
+	void create_titleLengthOver30_fail() {
+		assertThrows(InvalidArgumentException.class, () -> {
+			BookGroup.create(null, book, LocalDate.now(), LocalDate.now(), 2,
+				"작은 모임 작은 모임 작은 모임 작은 모임 작은 모임 작은 모임 작은 모임 작은 모임 작은 모임 작은 모임",
+				"작은 모임", true);
+		});
+	}
+
+	@DisplayName("create - book null일 경우 - 실패")
+	@Test
+	void create_bookNull_fail() {
+		assertThrows(InvalidArgumentException.class, () -> {
+			BookGroup.create(3L, null, LocalDate.now(), LocalDate.now(),
+				2, "작은 모임", "작은 모임", true);
+		});
+	}
+
+	@DisplayName("create - StartDate null일 경우 - 실패")
+	@Test
+	void create_startDateNull_fail() {
+		assertThrows(InvalidArgumentException.class, () -> {
+			BookGroup.create(3L, book, null, LocalDate.now(),
+				2, "작은 모임", "작은 모임", true);
+		});
+	}
+
+	@DisplayName("create - endDate null일 경우 - 실패")
+	@Test
+	void create_endDateNull_fail() {
+		assertThrows(InvalidArgumentException.class, () -> {
+			BookGroup.create(3L, book, LocalDate.now(), null,
+				2, "작은 모임", "작은 모임", true);
+		});
+	}
+
+	@DisplayName("create - startDate 지난 날짜인 경우 - 실패")
+	@Test
+	void create_startDateValid_fail() {
+		assertThrows(InvalidArgumentException.class, () -> {
+			BookGroup.create(3L, book, LocalDate.now().minusDays(1),
+				LocalDate.now(), 2, "작은 모임", "작은 모임", true);
+		});
+	}
+
+	@DisplayName("create - endDate startDate보다 빠를 경우 - 실패")
+	@Test
+	void create_endDateValid_fail() {
+		assertThrows(InvalidArgumentException.class, () -> {
+			BookGroup.create(3L, book, LocalDate.now().plusDays(1),
+				LocalDate.now().minusDays(1), 2, "작은 모임", "작은 모임",
+				true);
+		});
+	}
+
+	@DisplayName("create - introduce null일 경우 - 실패")
+	@Test
+	void create_introduceNull_fail() {
+		assertThrows(InvalidArgumentException.class, () -> {
+			BookGroup.create(3L, book, LocalDate.now(), LocalDate.now(),
+				2, "작은 모임", null, true);
+		});
+	}
+
+	@DisplayName("create - isPublic null일 경우 - 실패")
+	@Test
+	void create_isPublicNull_fail() {
+		assertThrows(InvalidArgumentException.class, () -> {
+			BookGroup.create(3L, book, LocalDate.now(), LocalDate.now(),
+				2, "작은 모임", "작은 모임", null);
+		});
+	}
+
+	@DisplayName("addComments - GroupComment가 null이 아니면 comments에 add 한다.")
+	@Test
+	void addComments() {
+		//given
+		Book book = BookObjectProvider.createAllFieldBook();
+		BookGroup bookGroup = BookGroupObjectProvider.createMockBookGroup(book,
+			1L);
+		User kakaoUser = UserObjectProvider.createKakaoUser();
+
+		GroupComment groupComment = GroupCommentObjectProvider.createComment("contents", bookGroup, kakaoUser);
+		//when
+		bookGroup.addComment(groupComment);
+
+		//then
+		assertEquals(1, bookGroup.getComments().size());
+		assertEquals(1L, bookGroup.getOwnerId());
+		assertEquals(book, bookGroup.getBook());
+		assertTrue(bookGroup.getComments().contains(groupComment));
+		assertEquals(groupComment.getBookGroup(), bookGroup);
+	}
+
+	@DisplayName("addComments - GroupComment가 null이 면 예외를 던진다.")
+	@Test
+	void addComments_throw() {
+		//given
+		Book book = BookObjectProvider.createAllFieldBook();
+		BookGroup bookGroup = BookGroupObjectProvider.createMockBookGroup(book,
+			1L);
+		GroupComment groupComment = null;
+		//when
+		assertThrows(InvalidArgumentException.class,
+			() -> bookGroup.addComment(groupComment));
+	}
+
+	@DisplayName("addMembers - GroupMember가 null이 아니면 members에 add 한다.")
+	@Test
+	void addMembers() {
+		//given
+		Book book = BookObjectProvider.createAllFieldBook();
+		BookGroup bookGroup = BookGroupObjectProvider.createMockBookGroup(book,
+			1L);
+		User kakaoUser = UserObjectProvider.createKakaoUser();
+
+		GroupMember groupMember = GroupMember.create(bookGroup, kakaoUser);
+
+		//when
+		bookGroup.addMember(groupMember);
+
+		//then
+		assertEquals(1, bookGroup.getGroupMembers().size());
+		assertEquals(1L, bookGroup.getOwnerId());
+		assertTrue(bookGroup.getGroupMembers().contains(groupMember));
+		assertEquals(bookGroup.getGroupMembers().get(0).getUser(), kakaoUser);
+		assertEquals(groupMember.getBookGroup(), bookGroup);
+	}
+
+	@DisplayName("addMembers - GroupMember가 null이 면 예외를 던진다.")
+	@Test
+	void addMembers_throw() {
+		//given
+		Book book = BookObjectProvider.createAllFieldBook();
+		BookGroup bookGroup = BookGroupObjectProvider.createMockBookGroup(book,
+			1L);
+		GroupMember groupMember = null;
+		//when
+		assertThrows(InvalidArgumentException.class,
+			() -> bookGroup.addMember(groupMember));
+	}
+
+}
