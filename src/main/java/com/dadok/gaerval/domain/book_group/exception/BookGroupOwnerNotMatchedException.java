39f3cbf4141f823253818125fd09dcfe1cb709ff
youngji <youngji.jang804@gmail.com>
2023-03-07 23:38:29 +0900

    chore : 커스텀 예외 에러코드 등록

--- src/main/java/com/dadok/gaerval/domain/book_group/exception/BookGroupOwnerNotMatchedException.java
+++ src/main/java/com/dadok/gaerval/domain/book_group/exception/BookGroupOwnerNotMatchedException.java
@@ -4,7 +4,11 @@ import com.dadok.gaerval.global.error.ErrorCode;
 import com.dadok.gaerval.global.error.exception.BusinessException;
 
 public class BookGroupOwnerNotMatchedException extends BusinessException {
+
+	private final ErrorCode errorCode = ErrorCode.BOOK_GROUP_OWNER_NOT_MATCHED;
+
 	public BookGroupOwnerNotMatchedException() {
 		super(ErrorCode.BOOK_GROUP_OWNER_NOT_MATCHED);
 	}
+
 }
