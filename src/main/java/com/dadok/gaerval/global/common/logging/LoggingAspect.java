66d610cfda0efc7485fb9f63d582422a11f05d67
Tina <migujeong@gmail.com>
2023-03-13 13:01:45 +0900

    chore : aop 로깅 package 명 생략

--- src/main/java/com/dadok/gaerval/global/common/logging/LoggingAspect.java
+++ src/main/java/com/dadok/gaerval/global/common/logging/LoggingAspect.java
@@ -17,16 +17,15 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class LoggingAspect {
 
-
 	@Before("@annotation(com.dadok.gaerval.global.common.logging.LogHttpRequests) && execution(public * *(..)))")
 	public void logHttpRequest(JoinPoint joinPoint) {
 		String controllerClassName = joinPoint.getTarget().getClass().getSimpleName();
-		MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+		MethodSignature methodSignature = (MethodSignature)joinPoint.getSignature();
 		String returnType = methodSignature.getReturnType().getSimpleName();
 		Type genericReturnType = methodSignature.getMethod().getGenericReturnType();
 		if (genericReturnType instanceof ParameterizedType) {
 			ParameterizedType parameterizedType;
-			parameterizedType = (ParameterizedType) genericReturnType;
+			parameterizedType = (ParameterizedType)genericReturnType;
 			Type[] actualTypeArguments = parameterizedType.getActualTypeArguments();
 			if (actualTypeArguments.length > 0 && actualTypeArguments[0] instanceof Class) {
 				returnType = ((Class)actualTypeArguments[0]).getSimpleName();
@@ -37,8 +36,12 @@ public class LoggingAspect {
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < parameters.length; i++) {
 			Parameter parameter = parameters[i];
-			Object value = joinPoint.getArgs()[i];
-			sb.append(parameter.getName()).append("=").append(value).append("|");
+			String value = joinPoint.getArgs()[i].toString();
+			String parameterName = parameter.getName();
+			if (value.startsWith("com.dadok")) {
+				value = value.substring(value.lastIndexOf('.') + 1);
+			}
+			sb.append(parameterName).append("=").append(value).append("|");
 		}
 
 		log.info("[{}]-[{}]-[{}]-[{}]", controllerClassName, returnType, methodName, sb);
