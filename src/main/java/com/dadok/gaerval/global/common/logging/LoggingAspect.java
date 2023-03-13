dafa661e821cfb4d048e3717aeb05df027f483e4
Tina <migujeong@gmail.com>
2023-03-13 09:50:49 +0900

    refact : AOP 로깅 시 실제 반환 타입 적용 하도록 변경

--- src/main/java/com/dadok/gaerval/global/common/logging/LoggingAspect.java
+++ src/main/java/com/dadok/gaerval/global/common/logging/LoggingAspect.java
@@ -1,7 +1,8 @@
 package com.dadok.gaerval.global.common.logging;
 
-import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
@@ -20,10 +21,19 @@ public class LoggingAspect {
 	@Before("@annotation(com.dadok.gaerval.global.common.logging.LogHttpRequests) && execution(public * *(..)))")
 	public void logHttpRequest(JoinPoint joinPoint) {
 		String controllerClassName = joinPoint.getTarget().getClass().getSimpleName();
-		String returnType = ((MethodSignature) joinPoint.getSignature()).getReturnType().getSimpleName();
-		Method method = ((MethodSignature) joinPoint.getSignature()).getMethod();
-		String methodName = method.getName();
-		Parameter[] parameters = method.getParameters();
+		MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+		String returnType = methodSignature.getReturnType().getSimpleName();
+		Type genericReturnType = methodSignature.getMethod().getGenericReturnType();
+		if (genericReturnType instanceof ParameterizedType) {
+			ParameterizedType parameterizedType;
+			parameterizedType = (ParameterizedType) genericReturnType;
+			Type[] actualTypeArguments = parameterizedType.getActualTypeArguments();
+			if (actualTypeArguments.length > 0 && actualTypeArguments[0] instanceof Class) {
+				returnType = ((Class)actualTypeArguments[0]).getSimpleName();
+			}
+		}
+		String methodName = methodSignature.getName();
+		Parameter[] parameters = methodSignature.getMethod().getParameters();
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < parameters.length; i++) {
 			Parameter parameter = parameters[i];
