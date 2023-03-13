73b3a7faeb33bb188e4863ccac0ac29fed8a3a6d
Tina <migujeong@gmail.com>
2023-03-13 11:56:53 +0900

    chore : slf4j annotation으로 변경

--- src/main/java/com/dadok/gaerval/global/common/logging/LoggingAspect.java
+++ src/main/java/com/dadok/gaerval/global/common/logging/LoggingAspect.java
@@ -8,15 +8,15 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Aspect
 @Component
+@Slf4j
 public class LoggingAspect {
 
-	private static final Logger log = LoggerFactory.getLogger(LoggingAspect.class);
 
 	@Before("@annotation(com.dadok.gaerval.global.common.logging.LogHttpRequests) && execution(public * *(..)))")
 	public void logHttpRequest(JoinPoint joinPoint) {
