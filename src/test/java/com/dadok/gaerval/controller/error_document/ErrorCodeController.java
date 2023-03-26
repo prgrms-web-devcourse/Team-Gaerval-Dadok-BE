e706538731602080be730b889da2d5e080e3239b
ysk <kim206gh@naver.com>
2023-03-26 18:34:07 +0900

    refactor : 에러 코드 컨트롤러 중복 코드 및 주석 제거

--- src/test/java/com/dadok/gaerval/controller/error_document/ErrorCodeController.java
+++ src/test/java/com/dadok/gaerval/controller/error_document/ErrorCodeController.java
@@ -8,9 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dadok.gaerval.global.error.ErrorCode;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -29,33 +27,11 @@ public class ErrorCodeController {
 		return map;
 	}
 
-	@JsonIgnoreProperties(ignoreUnknown = true)
-	@NoArgsConstructor
-	@AllArgsConstructor
-	@Getter
-	public static class ErrorCodeResponse {
-		private Map<ErrorCode, ErrorCodeMap> data;
-
-	}
-
-	// public record ErrorCodeResponse(
-	// 	Map<ErrorCode, ErrorCodeMap> data
-	// ) {
-	//
-	// 	public static ErrorCodeResponse of(Map<ErrorCode, ErrorCodeMap> data) {
-	// 		return new ErrorCodeResponse(data);
-	// 	}
-	//
-	// }
-
 	@Getter
 	@NoArgsConstructor
 	protected static class ErrorCodeMap {
-
 		private String code;
-
 		private String message;
-
 		private int status;
 
 		public ErrorCodeMap(ErrorCode errorCode) {
