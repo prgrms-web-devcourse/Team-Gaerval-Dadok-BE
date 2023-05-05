36111423040003dddb1f0b4c6030156ae529ccca
Tina <migujeong@gmail.com>
2023-05-05 13:21:28 +0900

    refact : mapstruct 테스트를 위한 컨텍스트 로딩 방식 변경 - @SpringbootTest 사용시 DB 연결 에러 발생 - 보다 가벼운 비용ㅇ로 테스트 하도록 변경

--- src/test/java/com/dadok/gaerval/domain/book/service/BookDataProcessTest.java
+++ src/test/java/com/dadok/gaerval/domain/book/service/BookDataProcessTest.java
@@ -8,11 +8,14 @@ import java.util.stream.Stream;
 
 import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.dadok.gaerval.domain.book.converter.BookMapper;
 import com.dadok.gaerval.domain.book.dto.response.OriginalBookData;
@@ -20,11 +23,10 @@ import com.dadok.gaerval.domain.book.entity.Book;
 import com.dadok.gaerval.global.config.externalapi.BookApiProvider;
 import com.dadok.gaerval.testutil.BookObjectProvider;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
 class BookDataProcessTest {
 
-	@Autowired
-	private BookMapper bookMapper;
+	private final BookMapper bookMapper = Mappers.getMapper(BookMapper.class);
 
 	@DisplayName("일반적이지 않은 데이터의 전처리에 성공한다.")
 	@ParameterizedTest
